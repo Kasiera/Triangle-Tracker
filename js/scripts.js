adb1cd85c87a49b730dd267cbbc81467b063d0f3
Samuel Kasiera <kasieras@gmail.com>
2018-08-10 18:27:40 +0300

    modify both js file and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,14 @@
 function triangleTracker() {
-
 var a = parseInt(document.getElementById('a'));
 var b = parseInt(document.getElementById('b'));
 var c = parseInt(document.getElementById('c'));
 
-var ab = a + b;
-var bc= b + c;
-var ca = c + a;
-if(ab >c || bc >a || ca >b) {
-alert("THIS IS NOT A TRIANGLE")
-}
-else if (a===b && b===c && c===a) {
+if (a===b && b===c && c===a) {
 alert("THIS IS AN EQUILATERAL TRIANGLE")
-} else if(a===b || b===c || c===a){
+} else if(a===b || b===c || c===a) {
 alert("THIS IS AN ISOSCELES")
-}
-else  {
+} else if(a+b <=c || b+c<=a || a+c<=b) {
+alert("THIS IS NOT A TRIANGLE")
+}else /*if (a!==b && b!==c && c!==a)*/ {
   alert ("This is a scalene triangle")
-  }
-}
+}};
