bddfe42fbfa72a1596f06d60ed6ed8a4087755b6
Samuel Kasiera <kasieras@gmail.com>
2018-08-13 17:37:19 +0300

    Changes made on the JS Code

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
 function triangleTracker() {
-var a = parseInt(document.getElementById('a'));
-var b = parseInt(document.getElementById('b'));
-var c = parseInt(document.getElementById('c'));
+var a = parseInt(document.getElementById('a').value);
+var b = parseInt(document.getElementById('b').value);
+var c = parseInt(document.getElementById('c').value);
 
-if (a===b && b===c && c===a) {
-alert("THIS IS AN EQUILATERAL TRIANGLE")
+if(a+b <c || b+c<a || a+c<b) {
+alert("This is NOT a triangle")
+}
+else if (a===b && b===c && c===a) {
+alert("This is an Equilateral Triangle")
 } else if(a===b || b===c || c===a) {
-alert("THIS IS AN ISOSCELES")
-} else if(a+b <=c || b+c<=a || a+c<=b) {
-alert("THIS IS NOT A TRIANGLE")
-}else /*if (a!==b && b!==c && c!==a)*/ {
-  alert ("This is a scalene triangle")
-}};
+alert("This is an Isosceles Triangle")
+}else if(a+b >c || b+c>a || a+c>b) /*if (a!==b && b!==c && c!==a)*/ {
+  alert ("This is a Scalene Triangle")
+}else {
+  alert("Input other values!");
+}
+};
+
+// //
+// // var a= prompt("Enter side A: ");
+// // var b= prompt("Enter side B: ");
+// // var c= prompt("Enter side C: ");
+//
+// if(a===b && a===c && b===c){
+//   alert("Equilateral");
+// }
+// else if(a===b || b===c || a===c){
+//   alert("Isosceles");
+// }
+// else if(a+b<=c || b+c<=a|| a+c<=b){
+//   alert("Not a Triangle");
+// }
+// else{
+//   alert("Scalar");
+// }
